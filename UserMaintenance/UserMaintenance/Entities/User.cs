fb13a806797b041cef0a00234a4206c998944be2
martonkol <marton.kolonics@stud.uni-corvinus.hu>
2022-09-29 18:07:28 +0200

    Comm

--- UserMaintenance/UserMaintenance/Entities/User.cs
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -9,8 +9,6 @@ namespace UserMaintenance.Entities
     public class User
     {
         public Guid ID { get; set; } = Guid.NewGuid();
-        public string FirstName;
-        public string LastName;
         public string FullName { get; set; }
 
     }
